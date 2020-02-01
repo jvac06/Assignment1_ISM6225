2dc5969b7fcd377a5ba6e39e3f2c95e12a9eaa2e
jesidacosta <jvac06@gmail.com>
2020-02-01 14:37:57 -0500

    Almost done with ex. 4

---
+++ ProblemThree.cs
@@ -0,0 +1,17 @@
+﻿/*using System;
+public class ProblemThree
+{
+	public string UsfTime()
+	{   
+        try
+        {
+            //Write your code here!!
+
+        }
+        catch
+        {
+            Console.WriteLine("Exception Occured while computing UsfTime");
+        }
+        return null;
+    }
+}*/
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Globalization;
+using System.Diagnostics;
 
 namespace Assignment1
 {
@@ -11,16 +13,16 @@ namespace Assignment1
             /*int num = 5;
             PrintPattern(num);*/
 
-            int n2 = 6;
-            PrintSeries(n2);
+            /*int n2 = 6;
+            PrintSeries(n2);*/
 
-            /*string s = "09:15:35PM";
+            /*string s = "12:30";
             string t = UsfTime(s);
             Console.WriteLine(t);*/
 
-           /* int n3 = 110;
+            int n3 = 110;
             int k = 11;
-            UsfNumbers(n3, k);*/
+            UsfNumbers(n3, k);
 
             /*string[] words = new string[] { "abcd", "dcba", "lls", "s", "sssll" };
             PalindromePairs(words);*/
@@ -84,7 +86,6 @@ namespace Assignment1
                     mySeries.Add(ans);
                     counter++;
                     num2--;
-                    
                 }
                 string printSeries = string.Join(",", mySeries);
                 Console.WriteLine(printSeries);
@@ -96,35 +97,108 @@ namespace Assignment1
 
         }
 
-        /*        public static string UsfTime(string s)
+
+        //Problem 3 
+        public static string UsfTime(string s)
+        {
+            /* On planet “USF” which is similar to that of Earth follows different clock
+             * where instead of hours they have U , instead of minutes they have S , instead
+             * of seconds they have F. Similar to earth where each day has 24 hours, each hour
+             * has 60 minutes and each minute has 60 seconds , USF planet’s day has 36 U , each
+             * U has 60 S and each S has 45 F. 
+             * Your task is to write a method usfTime which takes 12HR  format and return string 
+             * representing input time in USF time format.
+             * Input format: A string s with time in 12 hour clock format (i.e. hh:mm:ssAM or            * hh:mm:ssPM) where 01<= hh<=12, 00<=mm,ss,<=60
+             * Output format: a string with converted time in USF clock format (i.e. UU:SS:FF ) 
+             * where 01<= UU<=36, 00<=SS<=59,00<=FF<=45
+             * 
+             * Sample Input : 09:15:35PM
+             * Sample Output: 28:20:35 
+             * 
+             * returns      : String
+             * return type  : string
+             * 
+             * Hint: One way of doing this is by calculating total number of seconds in Input time
+             * and dividing those seconds according to USF time.
+             */
+            try
+            {
+                CultureInfo american = new CultureInfo("en-US");
+                DateTime earthTime = Convert.ToDateTime(s, american);
+                double hourToSec = (earthTime.Hour * 60) * 60;
+                double minToSec = (earthTime.Minute * 60);
+                double seconds = (earthTime.Second);
+                double inputTotalSec = hourToSec + minToSec + seconds;
+                double hourUSF = inputTotalSec / (45 * 60);
+                double minUSF = (hourUSF - Math.Truncate(hourUSF)) * 60;
+                double secUSF = (minUSF - Math.Truncate(minUSF)) * 45;
+                string result = $"{Math.Truncate(hourUSF).ToString("00.##", american)}:{Math.Truncate(minUSF).ToString("00.##", american)}:{Math.Round(secUSF).ToString("00.##", american)}";
+                return result;
+            }
+            catch
+            {
+                Console.WriteLine("Exception Occured while computing UsfTime");
+            }
+            return null;
+        }
+
+        //Problem 4
+        public static void UsfNumbers(int n3, int k)
+        {
+            /*n- total number of integers( 110 )
+             * k-number of numbers per line ( 11)
+             * USF Numbers : This method prints the numbers 1 to 110, 11 numbers per line.
+             * The method shall print 'U' in place of numbers which are multiple of 3,"S" for 
+             * multiples of 5,"F" for multiples of 7, 'US' in place of numbers which are multiple 
+             * of 3 and 5,'SF' in place of numbers which are multiple of 5 and 7 and so on. 
+             * The output shall look like 
+             * 1 2 U 4 S U F 8 U S 11 
+             * U 13 F US 16 17 U 19 S UF 22
+             * 23 U S 26 U F 29 US 31 32 U....
+             * 
+             * returns      : N/A
+             * return type  : void
+             */
+
+            try
+            {
+                // Write your code here
+                int[] totalInt = Enumerable.Range(0, n3).ToArray();
+                for (int i = 1; i <= totalInt.Length; i++)
                 {
-                    try
+                    string output;
+                    if (i % 3 == 0)
                     {
-                        //Write your code here .!!
+                        output = "U";
                     }
-                    catch
+                    else if (i % 5 == 0)
                     {
-                        Console.WriteLine("Exception Occured while computing UsfTime");
+                        output = "S";
                     }
-                    return null;
+                    else if (i % 7 == 0)
+                    {
+                        output = "F";
+                    }
+                    else
+                    {
+                        output = i.ToString();
+                    }
+                    Console.Write(output + " ");
+                    if (i % 11 == 0)
+                    {
+                        Console.Write(Environment.NewLine);
+                    }
                 }
-
-
-                public static void UsfNumbers(int n3, int k)
-                {
-                    try
-                    {
-                        // Write your code here
-                    }
-                    catch
-                    {
-                        Console.WriteLine("Exception occured while computing UsfNumbers()");
-                    }
-                }
+            }
+            catch
+            {
+                Console.WriteLine("Exception occured while computing UsfNumbers()");
+            }
+        }
 
 
 
-                public static void PalindromePairs(string[] words)
+        /*        public static void PalindromePairs(string[] words)
                 {
                     try
                     {
@@ -135,9 +209,9 @@ namespace Assignment1
 
                         Console.WriteLine("Exception occured while computing     PalindromePairs()");
                     }
-                }
+                }*/
 
-                public static void Stones(int n4)
+        /*        public static void Stones(int n4)
                 {
                     try
                     {
@@ -146,8 +220,8 @@ namespace Assignment1
                     catch
                     {
                         Console.WriteLine("Exception occured while computing Stones()");
-                    }
-                }
-        */
+                    }*/
+
+
     }
 }
